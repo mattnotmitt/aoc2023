4f82004c639c41934b7f67a317a83bfe85ef395c
Matt C <me@mitt.dev>
2023-12-02 09:36:48 +0000

    day2 python

---
+++ python/day2/day2.py
@@ -0,0 +1,50 @@
+from collections import defaultdict
+import functools
+import operator
+
+# INPUT = "day2/day2_example.txt"
+INPUT = "day2/day2.txt"
+
+def checkGame(rounds, maxVal) -> int:
+    [gameId, rounds] = rounds.split(": ")
+
+    for round in rounds.split("; "):
+        for count in round.split(", "):
+            [num, colour] = count.split(" ")
+            if int(num) > maxVal[colour]:
+                return 0
+            
+    return int(gameId.split(" ")[1])
+
+
+def part1(input: list[str], maxVal: dict[str, int] = {"red": 12, "green": 13, "blue": 14}) -> int:
+    id_sum = 0
+    for game in input:
+        id_sum += checkGame(game, maxVal)
+
+    return id_sum
+
+def getPowers(rounds) -> int:
+    [_, rounds] = rounds.split(": ")
+
+    maxVals = defaultdict(lambda: 0)
+
+    for round in rounds.split("; "):
+        for count in round.split(", "):
+            [num, colour] = count.split(" ")
+            if int(num) > maxVals[colour]:
+                maxVals[colour] = int(num)
+    
+    return functools.reduce(operator.mul, maxVals.values())
+
+def part2(input: list[str]) -> int:
+    power_sum = 0
+    for game in input:
+        power_sum += getPowers(game)
+    return power_sum
+
+if __name__ == '__main__':
+    with open(INPUT) as f:
+        data = f.read().splitlines()
+        print(f"Part1: sum of game ids which are valid: {part1(data)}")
+        print(f"Part2: sum of all game powers: {part2(data)}")
